669f1ff98f0edb7185080a3fa5bf1315af8ea24c
ashraful-hoda <asrafulhoda789@gmail.com>
2026-03-03 03:00:01 +0600

    how to send fetched data to another function

--- Module-28(API,JSON,Data Load, Dynamic website))/script.js
+++ Module-28(API,JSON,Data Load, Dynamic website))/script.js
@@ -1,9 +1,26 @@
 
 const loadData = () => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then((res) => res.json())
+    fetch('https://jsonplaceholder.typicode.com/todos/1') // promise of response
+    .then((res) => res.json()) // promise of json data
     .then((data) => console.log(data))
 };
+
+const loadPost = () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        displayPost(data);
+    })
+};
+
+const displayPost = (posts) =>{
+    posts.forEach(post => {
+        console.log(post);
+    });
+}
 
 
 
