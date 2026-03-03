1002d5f63d67c3f59797621e5c5893308089e3df
ashraful-hoda <asrafulhoda789@gmail.com>
2026-03-03 19:44:39 +0600

    async await function

---
+++ Module-28(API,JSON,Data Load, Dynamic website))/test.js
@@ -0,0 +1,13 @@
+
+
+
+  const data = async() => {
+    const res  = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+    console.log("Hello");
+    const json = await res.json();
+    console.log(json);
+    console.log(true);
+  } 
+
+  data();
+
